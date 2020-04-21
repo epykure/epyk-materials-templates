ffd4b4a24e34a60578c9c69f55e78a454a1c03ec
marlyk <leemark0071@gmail.com>
2020-04-21 19:26:09 +0100

    Update chips.py

--- basics/chips.py
+++ basics/chips.py
@@ -7,6 +7,11 @@ import config
 # Create a basic report object
 rptObj = Report()
 
-rptObj.materials.texts.chips("test")
+rptObj.materials.texts.chip("test")
+rptObj.materials.texts.chip(["test %s" % i for i in range(5)])
+
+rptObj.materials.texts.chip(["test %s" % i for i in range(5)], choice=True)
+
+rptObj.materials.inputs.chip(["test %s" % i for i in range(5)])
 
 rptObj.outs.html_file(path=config.OUTPUT_PATHS_LOCALS_HTML, name=config.OUT_FILENAME)
