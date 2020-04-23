c1015b00a28ec71c93da47fbd2881cc69a353e3a
marlyk <leemark0071@gmail.com>
2020-04-23 12:25:17 +0100

    Update input.py

--- basics/input.py
+++ basics/input.py
@@ -6,9 +6,22 @@ import config
 # Create a basic report object
 rptObj = Report()
 
-#rptObj.materials.inputs.textarea("", "favorite")
-#rptObj.materials.inputs.input("", "favorite")
-#rptObj.materials.inputs.filled("", "favorite")
-#rptObj.materials.inputs.outlined("", "favorite")
+rptObj.materials.inputs.textarea("", "favorite")
+i = rptObj.materials.inputs.input("", "favorite")
 
+p = rptObj.materials.inputs.password("", "password")
+p = rptObj.materials.inputs.prefilled("data", "password")
+
+i += rptObj.materials.icon("favorite")
+
+#rptObj.materials.inputs.filled("", "favorite")
+rptObj.materials.inputs.outlined("", "favorite")
+text_1 = rptObj.materials.inputs.outlined("Test", "Title", leading_icon="favorite")
+text_2 = rptObj.materials.inputs.outlined("Test", "Title", trailing_icon="visibility")
+
+rptObj.ui.button("Click").click([
+  p.dom.setDisabled(True),
+  rptObj.js.console.log(i.dom.content),
+  text_1.dom.setValue(i.dom.content),
+])
 rptObj.outs.html_file(path=config.OUTPUT_PATHS_LOCALS_HTML, name=config.OUT_FILENAME)
