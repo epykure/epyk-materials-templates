ade75a38f698dbbe4f97f0b1c64f25b7c76199f3
Tinelsi972 <epykachu@gmail.com>
2020-04-24 23:42:29 +0100

    add checkbox_templates

--- basics/checkbox.py
+++ basics/checkbox.py
@@ -5,7 +5,10 @@ import config
 
 # Create a basic report object
 rptObj = Report()
-rptObj.materials.inputs.checkbox(True, "test")
-
+chk1 = rptObj.materials.inputs.checkbox(True, "test")
+chk2 = rptObj.materials.inputs.checkbox(False, "test2")
+chk3 = rptObj.materials.inputs.checkbox(False, "test3")
+rptObj.js.addOnReady([chk3.dom.setStatus('indeterminate')])
 
 rptObj.outs.html_file(path=config.OUTPUT_PATHS_LOCALS_HTML, name=config.OUT_FILENAME)
+
